1e6751a318a1b355d831a033c5e9a8d34569a2f3
Jinny Sung <75344406+sjinicd@users.noreply.github.com>
2021-06-13 14:59:52 +0900

    Create 2021.06.13.js

---
+++ 05月/2021.06.13.js
@@ -0,0 +1,15 @@
+<div id="test">TEST</div>
+ 
+<script type="text/javascript">
+alert(document.getElementById('test').innerHTML);
+//결과는 TEST
+ 
+alert(document.getElementById('test').outerHTML);
+//결과는 <div id="test">TEST</div>
+
+</script>
+</body>
+ 
+
+
+출처: https://na27.tistory.com/226 [na27]
