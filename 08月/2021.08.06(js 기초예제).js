6c550528aa45381d3b3f29c89ca243fcb5376095
Jinny Sung <75344406+sjinicd@users.noreply.github.com>
2021-08-06 09:20:13 +0900

    Create 2021.08.06(js 기초예제).js

---
+++ 08月/2021.08.06(js 기초예제).js
@@ -0,0 +1,25 @@
+
+var test = {
+  "1" : {chat_no : 10, last_chat_no : 4},
+  "2" : {chat_no : 11, last_chat_no : 4},
+  "3" : {chat_no : 11, last_chat_no : 4}
+}
+
+console.log("!@#!@#", test[1])
+// 결과
+!@#!@#
+{
+chat_no:10,
+last_chat_no:4
+}
+
+
+var test = {
+  "1" : {chat_no : 10, last_chat_no : 4},
+  "2" : {chat_no : 11, last_chat_no : 4},
+  "3" : {chat_no : 11, last_chat_no : 4}
+}
+
+console.log("!@#!@#", test[5])
+//결과
+!@#!@#undefined
