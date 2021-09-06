7655a736987b20ae763ec5cb395442994522ae6e
Jinny Sung <75344406+sjinicd@users.noreply.github.com>
2021-09-06 10:51:11 +0900

    Create for문안에서 배열값 밖에서 쓰기.js

---
+++ 09月/for문안에서 배열값 밖에서 쓰기.js
@@ -0,0 +1,8 @@
+var chatSupport = document.getElementsByClassName('chatSupport')
+		    var chatSupportName = "";
+		    var i;
+		      for (i = 0; i < chatSupport.length; i++) {
+		        //console.log("chatSupport[i]",chatSupport[i])
+		        chatSupportName = chatSupport[i]
+		      }
+		      console.log("chatSupportName1",chatSupportName)
